2823750d432c1e67760c80131e1c63295d0c7204
trevinbrooks <10768859+trevinbrooks@users.noreply.github.com>
2023-04-27 21:08:00 -0500

    better handle destination config names

--- CF.AccessProxy/Config/Validation/DestinationHelper.cs
+++ CF.AccessProxy/Config/Validation/DestinationHelper.cs
@@ -1,4 +1,3 @@
-using System.Collections.ObjectModel;
 using System.Text.RegularExpressions;
 using Yarp.ReverseProxy.Configuration;
 
@@ -8,12 +7,12 @@ public partial class DestinationHelper
 {
     /// <summary>
     /// Converts a semicolon separated string of urls to a dictionary of destinations.
-    /// Hostname is used as the key.
+    /// Keys are the subdomain, host, or full url depending on which is unique/available.
     /// </summary>
     /// <exception cref="ArgumentException">If the string is null or whitespace.</exception>
     public static Dictionary<string, DestinationConfig> ConvertStringToDestinations(string semicolonSeparatedUrls)
     {
-        if (string.IsNullOrWhiteSpace(semicolonSeparatedUrls)) 
+        if (string.IsNullOrWhiteSpace(semicolonSeparatedUrls))
         {
             throw new ArgumentException("The argument cannot be null or whitespace.", nameof(semicolonSeparatedUrls));
         }
@@ -25,7 +24,14 @@ public partial class DestinationHelper
         {
             var trimmedUrl = url.Trim();
             var subdomain = Subdomain(trimmedUrl);
-            destinations.Add(subdomain, new DestinationConfig { Address = trimmedUrl });
+            var config = new DestinationConfig { Address = trimmedUrl };
+
+            if (destinations.TryAdd(subdomain, config))
+                continue;
+            if (destinations.TryAdd(new Uri(trimmedUrl).Host, config))
+                continue;
+            if (!destinations.TryAdd(trimmedUrl, config))
+                throw new ArgumentException($"The URL '{trimmedUrl}' is a duplicate.", nameof(semicolonSeparatedUrls));
         }
 
         return destinations;
@@ -34,9 +40,9 @@ public partial class DestinationHelper
     private static string Subdomain(string url)
     {
         var subdomain = SubdomainRegex().Match(url).Groups["subdomain"].Value;
-        return string.IsNullOrWhiteSpace(subdomain) ? url : subdomain;
+        return string.IsNullOrWhiteSpace(subdomain) ? new Uri(url).Host : subdomain;
     }
-    
+
     [GeneratedRegex(@"^https?://(?<subdomain>[^.]+)\..+$", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
     private static partial Regex SubdomainRegex();
 }
